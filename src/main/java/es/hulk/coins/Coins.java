34109afa23eb4fd5a016d0b2888290b40d23614d
Miquel Angel <hulkrojo2@gmail.com>
2022-01-20 12:13:10 +0100

    Initial commit

---
+++ src/main/java/es/hulk/coins/Coins.java
@@ -0,0 +1,17 @@
+package es.hulk.coins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Coins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
