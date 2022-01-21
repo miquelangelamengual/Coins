05d75c9c189b864f431f0dd54022ea7fc50ebe6a
Miquel Angel <hulkrojo2@gmail.com>
2022-01-21 11:05:57 +0100

    Removed config. custom plugin dont need configs

--- src/main/java/es/hulk/coins/Coins.java
+++ src/main/java/es/hulk/coins/Coins.java
@@ -2,7 +2,6 @@ package es.hulk.coins;
 
 import es.hulk.coins.commands.CoinsCommand;
 import es.hulk.coins.commands.CoinsMenuCommand;
-import es.hulk.coins.utils.FileConfig;
 import es.hulk.coins.utils.command.CommandManager;
 import es.hulk.coins.utils.menu.ButtonListener;
 import lombok.Getter;
@@ -17,11 +16,9 @@ import java.util.ArrayList;
 public class Coins extends JavaPlugin {
 
     private CommandManager commandManager;
-    private FileConfig mainConfig;
 
     @Override
     public void onEnable() {
-        this.mainConfig = new FileConfig(this, "config.yml");
         this.commandManager = new CommandManager(this, new ArrayList<>());
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(new ButtonListener(), this);
