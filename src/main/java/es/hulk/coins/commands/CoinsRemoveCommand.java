82be009a857cd8775335ea872f341e4f5f8f9ee9
Miquel Angel <hulkrojo2@gmail.com>
2022-01-21 12:25:59 +0100

    Added command to remove coins

--- src/main/java/es/hulk/coins/commands/CoinsRemoveCommand.java
+++ src/main/java/es/hulk/coins/commands/CoinsRemoveCommand.java
@@ -1,5 +1,12 @@
 package es.hulk.coins.commands;
 
+import es.hulk.coins.utils.AquaCoreUtils;
+import es.hulk.coins.utils.command.BaseCommand;
+import es.hulk.coins.utils.command.Command;
+import es.hulk.coins.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
 /**
  * Created by Hulk
  * at 20/01/2022
@@ -7,5 +14,39 @@ package es.hulk.coins.commands;
  * Class: CoinsRemoveCommand
  */
 
-public class CoinsRemoveCommand {
+public class CoinsRemoveCommand extends BaseCommand {
+
+    @Command(name = "coins.remove", permission = "coins.command.remove")
+
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length == 1) {
+            try {
+                int amount = Integer.parseInt(args[0]);
+                AquaCoreUtils.removeCoins(player, amount);
+                player.sendMessage("§aYou have removed §f" + amount + " §acoins to your account.");
+            } catch (NumberFormatException e) {
+                player.sendMessage("§cInvalid number.");
+            }
+        }
+
+        if (args.length == 2) {
+            try {
+                Player target = Bukkit.getPlayer(args[0]);
+                int amount = Integer.parseInt(args[1]);
+
+                if (target != null) {
+                    AquaCoreUtils.removeCoins(target, amount);
+                    player.sendMessage("You have removed " + amount + " coins to " + args[0]);
+                } else {
+                    player.sendMessage("§cPlayer not found.");
+                }
+            } catch (NumberFormatException e) {
+                player.sendMessage("§cInvalid number.");
+            }
+        }
+    }
 }
