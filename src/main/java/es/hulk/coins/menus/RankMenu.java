281c5587e84c92b6efc310a51c135b27b02c49e8
Miquel Angel <hulkrojo2@gmail.com>
2022-01-21 12:26:59 +0100

    Added Rank menu that you can select which type of rank you wanna get.

--- src/main/java/es/hulk/coins/menus/RankMenu.java
+++ src/main/java/es/hulk/coins/menus/RankMenu.java
@@ -1,4 +1,59 @@
 package es.hulk.coins.menus;
 
-public class RankMenu {
+import es.hulk.coins.utils.ItemBuilder;
+import es.hulk.coins.utils.menu.Button;
+import es.hulk.coins.utils.menu.Menu;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RankMenu extends Menu {
+
+    @Override
+    public String getTitle(Player player) {
+        return "&6&lRANKS";
+    }
+
+    @Override
+    public int size(Map<Integer, Button> buttons) {
+        return 27;
+    }
+
+    @Override
+    public Map<Integer, Button> getButtons(Player player) {
+        Map<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(11, new Button() {
+
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.DIAMOND_SWORD).name("&6&lPermanent Ranks").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                
+            }
+        });
+
+        buttons.put(15, new Button() {
+
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.IRON_SWORD).name("&6&lTemporal Ranks").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+
+            }
+        });
+
+
+        return buttons;
+    }
 }
---
+++ src/main/java/es/hulk/coins/menus/rank/PermRankMenu.java
@@ -0,0 +1,4 @@
+package es.hulk.coins.menus.rank;
+
+public class PermRankMenu {
+}
