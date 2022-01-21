135c8677b602f564eceed702536e022b14d6b48c
Miquel Angel <hulkrojo2@gmail.com>
2022-01-21 11:02:09 +0100

    Added CoinsCommand

--- src/main/java/es/hulk/coins/commands/CoinsCommand.java
+++ src/main/java/es/hulk/coins/commands/CoinsCommand.java
@@ -1,5 +1,11 @@
 package es.hulk.coins.commands;
 
+import es.hulk.coins.utils.AquaCoreUtils;
+import es.hulk.coins.utils.command.BaseCommand;
+import es.hulk.coins.utils.command.Command;
+import es.hulk.coins.utils.command.CommandArgs;
+import org.bukkit.entity.Player;
+
 /**
  * Created by Hulk
  * at 20/01/2022
@@ -7,5 +13,14 @@ package es.hulk.coins.commands;
  * Class: CoinsCommand
  */
 
-public class CoinsCommand {
+public class CoinsCommand extends BaseCommand {
+
+    @Command(name = "coins", aliases = {"coin", "money"}, usage = "coins", description = "Shows the amount of coins you have.")
+
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+
+        player.sendMessage("§aYou have §e" + AquaCoreUtils.getCoins(player) + " §acoins.");
+    }
 }
