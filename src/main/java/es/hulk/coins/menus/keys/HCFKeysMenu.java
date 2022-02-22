ca4b7def1362e77482fda1b61492468dda19481c
Miquel Angel <hulkrojo2@gmail.com>
2022-02-22 15:31:25 +0100

    Added menu for HCF Keys

---
+++ src/main/java/es/hulk/coins/menus/keys/HCFKeysMenu.java
@@ -0,0 +1,35 @@
+package es.hulk.coins.menus.keys;
+
+import es.hulk.coins.utils.menu.Button;
+import es.hulk.coins.utils.menu.Menu;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Hulk
+ * at 22/02/2022 15:24
+ * Project: Coins
+ * Class: HCFKeysMenu
+ */
+
+public class HCFKeysMenu extends Menu {
+
+    @Override
+    public String getTitle(Player player) {
+        return "HCF Keys";
+    }
+
+    @Override
+    public int size(Map<Integer, Button> buttons) {
+        return 27;
+    }
+
+    @Override
+    public Map<Integer, Button> getButtons(Player player) {
+        Map<Integer, Button> buttons = new HashMap<>();
+
+        return buttons;
+    }
+}
