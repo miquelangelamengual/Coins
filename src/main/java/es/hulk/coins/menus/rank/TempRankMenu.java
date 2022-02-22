14d8989b3275a22d4d81ce62c4d2728c65b4dc65
Miquel Angel <hulkrojo2@gmail.com>
2022-02-22 10:36:33 +0100

    added rank selection to temp rank menu

--- src/main/java/es/hulk/coins/menus/rank/TempRankMenu.java
+++ src/main/java/es/hulk/coins/menus/rank/TempRankMenu.java
@@ -1,10 +1,15 @@
 package es.hulk.coins.menus.rank;
 
 import es.hulk.coins.menus.CoinsMenu;
+import es.hulk.coins.utils.ItemBuilder;
+import es.hulk.coins.utils.aquacore.RankUtils;
 import es.hulk.coins.utils.menu.Button;
 import es.hulk.coins.utils.menu.Menu;
 import es.hulk.coins.utils.menu.buttons.BackButton;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +29,66 @@ public class TempRankMenu extends Menu {
     @Override
     public Map<Integer, Button> getButtons(Player player) {
         Map<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(11, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bEtoirir").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setTempRank(player, "Etoirir");
+            }
+        });
+
+        buttons.put(12, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bBlaze").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setTempRank(player, "Blaze");
+            }
+        });
+
+        buttons.put(13, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bRex").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setTempRank(player, "Rex");
+            }
+        });
+
+        buttons.put(14, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bErrier").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setTempRank(player, "Errier");
+            }
+        });
+
+        buttons.put(15, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bCrowl").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setTempRank(player, "Crowl");
+            }
+        });
 
         buttons.put(22, new BackButton(new CoinsMenu()));
 
