7f6dd398060b51132274afe2d69f9b0e94934686
Miquel Angel <hulkrojo2@gmail.com>
2022-02-22 10:36:12 +0100

    added ranks to perm rank menu

--- src/main/java/es/hulk/coins/menus/rank/PermRankMenu.java
+++ src/main/java/es/hulk/coins/menus/rank/PermRankMenu.java
@@ -1,10 +1,17 @@
 package es.hulk.coins.menus.rank;
 
 import es.hulk.coins.menus.CoinsMenu;
+import es.hulk.coins.utils.ItemBuilder;
+import es.hulk.coins.utils.Utils;
+import es.hulk.coins.utils.aquacore.CoinsUtils;
+import es.hulk.coins.utils.aquacore.RankUtils;
 import es.hulk.coins.utils.menu.Button;
 import es.hulk.coins.utils.menu.Menu;
 import es.hulk.coins.utils.menu.buttons.BackButton;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +31,75 @@ public class PermRankMenu extends Menu {
     @Override
     public Map<Integer, Button> getButtons(Player player) {
         Map<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(11, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bEtoirir").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                if (CoinsUtils.getCoins(player) >= 1000) {
+                    if (RankUtils.hasRank(player, "Etoirir")) {
+                        RankUtils.setPermRank(player, "Etoirir");
+                        CoinsUtils.removeCoins(player, 1000);
+                    } else {
+                        player.sendMessage(Utils.color("&cYou have a higher or equal rank!"));
+                    }
+                } else {
+                    player.sendMessage(Utils.color("&cYou don't have enough coins!"));
+                }
+            }
+        });
+
+        buttons.put(12, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bBlaze").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setPermRank(player, "Blaze");
+            }
+        });
+
+        buttons.put(13, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bRex").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setPermRank(player, "Rex");
+            }
+        });
+
+        buttons.put(14, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bErrier").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setPermRank(player, "Errier");
+            }
+        });
+
+        buttons.put(15, new Button() {
+            @Override
+            public ItemStack getButtonItem(Player p0) {
+                return new ItemBuilder(Material.PAPER).name("&bCrowl").build();
+            }
+
+            @Override
+            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                RankUtils.setPermRank(player, "Crowl");
+            }
+        });
 
         buttons.put(22, new BackButton(new CoinsMenu()));
 
