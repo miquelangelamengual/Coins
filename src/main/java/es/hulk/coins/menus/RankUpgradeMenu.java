97e3f0401ce689149efb0658262f8511ad6b8256
Edwi <edwicitoo@gmail.com>
2022-03-11 15:37:43 -0600

    Now xd

--- src/main/java/es/hulk/coins/menus/RankUpgradeMenu.java
+++ src/main/java/es/hulk/coins/menus/RankUpgradeMenu.java
@@ -36,14 +36,14 @@ public class RankUpgradeMenu extends Menu {
         buttons.put(11, new Button() {
             @Override
             public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER).name("Etoirir » Blaze").lore(Arrays.asList("", "", "")).build();
+                return new ItemBuilder(Material.PAPER).name("&9Etoirir &8» &cBlaze").lore(Arrays.asList("", "&fThis rank upgrade is &cpermanent&7.", "", "&fCost: &6⛃250 coins", "", "&7&oSee the advantages of this rank", "", "&7&oin crowl.club/discord &7-&o ranks channel", "&a&lClick to purchase it")).build();
             }
 
             @Override
             public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                if (CoinsUtils.getCoins(player) >= 10) {
+                if (CoinsUtils.getCoins(player) >= 250) {
                     RankUtils.setPermRank(player, "Blaze");
-                    CoinsUtils.removeCoins(player, 10);
+                    CoinsUtils.removeCoins(player, 250);
                     return;
                 }
                 player.sendMessage(Utils.color("You dont have enought coins to buy this."));
@@ -53,14 +53,14 @@ public class RankUpgradeMenu extends Menu {
         buttons.put(12, new Button() {
             @Override
             public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER).name("Blaze » Rex").lore(Arrays.asList("", "", "")).build();
+                return new ItemBuilder(Material.PAPER).name("Blaze » Rex").lore(Arrays.asList("", "&fThis rank upgrade is &cpermanent&7.", "", "&fCost: &6⛃350 coins", "", "&7&oSee the advantages of this rank", "", "&7&oin crowl.club/discord &7-&o ranks channel", "&a&lClick to purchase it")).build();
             }
 
             @Override
             public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                if (CoinsUtils.getCoins(player) >= 10) {
+                if (CoinsUtils.getCoins(player) >= 350) {
                     RankUtils.setPermRank(player, "Rex");
-                    CoinsUtils.removeCoins(player, 10);
+                    CoinsUtils.removeCoins(player, 350);
                     return;
                 }
                 player.sendMessage(Utils.color("You dont have enought coins to buy this."));
@@ -70,14 +70,14 @@ public class RankUpgradeMenu extends Menu {
         buttons.put(14, new Button() {
             @Override
             public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER).name("Rex » Errier").lore(Arrays.asList("", "", "")).build();
+                return new ItemBuilder(Material.PAPER).name("Rex » Errier").lore(Arrays.asList("", "&fThis rank upgrade is &cpermanent&7.", "", "&fCost: &6⛃620 coins", "", "&7&oSee the advantages of this rank", "", "&7&oin crowl.club/discord &7-&o ranks channel", "&a&lClick to purchase it")).build();
             }
 
             @Override
             public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                if (CoinsUtils.getCoins(player) >= 10) {
+                if (CoinsUtils.getCoins(player) >= 620) {
                     RankUtils.setPermRank(player, "Errier");
-                    CoinsUtils.removeCoins(player, 10);
+                    CoinsUtils.removeCoins(player, 620);
                     return;
                 }
                 player.sendMessage(Utils.color("You dont have enought coins to buy this."));
@@ -87,14 +87,14 @@ public class RankUpgradeMenu extends Menu {
         buttons.put(15, new Button() {
             @Override
             public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER).name("Errier » Crowl").lore(Arrays.asList("", "", "")).build();
+                return new ItemBuilder(Material.PAPER).name("Errier » Crowl").lore(Arrays.asList("", "&fThis rank upgrade is &cpermanent&7.", "", "&fCost: &6⛃850 coins", "", "&7&oSee the advantages of this rank", "", "&7&oin crowl.club/discord &7-&o ranks channel", "&a&lClick to purchase it")).build();
             }
 
             @Override
             public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                if (CoinsUtils.getCoins(player) >= 10) {
+                if (CoinsUtils.getCoins(player) >= 850) {
                     RankUtils.setPermRank(player, "Crowl");
-                    CoinsUtils.removeCoins(player, 10);
+                    CoinsUtils.removeCoins(player, 850);
                     return;
                 }
                 player.sendMessage(Utils.color("You dont have enought coins to buy this."));
