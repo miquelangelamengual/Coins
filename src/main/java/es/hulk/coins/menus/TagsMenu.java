7f80ae9aa0b912e7442b008bbedb3565d1a83c47
Miquel Angel <hulkrojo2@gmail.com>
2022-04-27 11:30:18 +0200

    started tag menu

--- src/main/java/es/hulk/coins/menus/TagsMenu.java
+++ src/main/java/es/hulk/coins/menus/TagsMenu.java
@@ -1,8 +1,17 @@
 package es.hulk.coins.menus;
 
+import es.hulk.coins.utils.ItemBuilder;
+import es.hulk.coins.utils.aquacore.CoinsUtils;
 import es.hulk.coins.utils.menu.Button;
 import es.hulk.coins.utils.menu.Menu;
+import es.hulk.coins.utils.menu.pagination.PaginatedMenu;
+import me.activated.core.api.tags.Tag;
+import me.activated.core.plugin.AquaCore;
+import me.activated.core.plugin.AquaCoreAPI;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +23,7 @@ import java.util.Map;
  * Class: TagsMenu
  */
 
-public class TagsMenu extends Menu {
+public class TagsMenu extends PaginatedMenu {
     @Override
     public String getTitle(Player player) {
         return "Tags";
@@ -26,9 +35,30 @@ public class TagsMenu extends Menu {
     }
 
     @Override
-    public Map<Integer, Button> getButtons(Player player) {
+    public String getPrePaginatedTitle(Player p0) {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Button> getAllPagesButtons(Player p0) {
         Map<Integer, Button> buttons = new HashMap<>();
 
-        return buttons;
+        for (Tag tag : AquaCore.INSTANCE.getTagManagement().getTags()) {
+            buttons.put(buttons.size(), new Button() {
+                @Override
+                public ItemStack getButtonItem(Player p0) {
+                    return new ItemBuilder(Material.NAME_TAG).name(tag.getColor() + tag.getName()).build();
+                }
+
+                @Override
+                public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+                    if (CoinsUtils.getCoins(player) >= 350) {
+
+                    }
+                }
+            });
+        }
+
+        return null;
     }
 }
